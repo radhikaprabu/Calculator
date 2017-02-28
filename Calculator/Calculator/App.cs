72c1ba2d01ea50c7f8f003f0e0c40c7028f81e96
radhikaprabu <radhikaprabu91699@gmail.com>
2017-02-27 18:07:09 -0600

    Add project files.

---
+++ Calculator/Calculator/App.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public class App : Application
+    {
+        const string displayLabelText = "displayLabelText";
+
+        public string DisplayLabelText { get; set; }
+
+        public App()
+        {
+            if (Properties.ContainsKey(displayLabelText))
+            {
+                DisplayLabelText = (string)Properties[displayLabelText];
+            }
+
+            MainPage = new CalculatorPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Calculator/Calculator/CalculatorPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorPage 
+    {
+        App app = Application.Current as App;
+        List<int> numbers = new List<int>();
+        float leftNumber = 0;
+        float rightNumber = 0;
+        String operation = "";
+        public CalculatorPage()
+        {
+            InitializeComponent();
+
+            displayLabel.Text = app.DisplayLabelText;
+        }
+
+        void OnDigitButtonClicked(object sender, EventArgs args)
+        {
+            String key = ((Button)sender).Text;
+            displayLabel.Text += key;
+        }
+        void OnOperationButtonClicked(object sender, EventArgs args)
+        {
+            String key = ((Button)sender).Text;
+            leftNumber = float.Parse(displayLabel.Text);
+            displayLabel.Text = "";
+            operation = key;
+        }
+        void OnEqualButtonClicked(object sender, EventArgs args)
+        {
+            rightNumber = float.Parse(displayLabel.Text);
+            switch (operation)
+                {
+                case "+": displayLabel.Text = "" + (leftNumber + rightNumber);
+                    break;
+                case "-": displayLabel.Text = "" + (leftNumber - rightNumber);
+                    break;
+                case "/":
+                    displayLabel.Text = "" + (leftNumber / rightNumber);
+                    break;
+                case "*":
+                    displayLabel.Text = "" + (leftNumber * rightNumber);
+                    break;
+                default: break;
+                }
+            leftNumber = 0;
+            rightNumber = 0;
+            operation = null;
+        }
+        void OnClearButtonClicked(object sender, EventArgs args)
+        {
+            displayLabel.Text = "";
+            leftNumber = 0;
+            rightNumber = 0;
+            operation = null;
+        }
+    }
+}
